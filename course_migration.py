edfc4958daa95d6566e56c33f42f6b9ecf84e399
popcat <moviemaker7@gmail.com>
2015-09-17 21:17:00 -0400

    finished the pasing-csv part of the migration script. will add the actual querying part once db is up.

---
+++ course_migration.py
@@ -0,0 +1,61 @@
+'''
+Evan Hopkins
+Sept. 17 2105
+BBEMT Capping Team
+'''
+import csv
+
+def isSubjectValid(course_name):
+	if (any(char.isdigit() for char in course_name) ):
+		return False
+	return True
+
+
+# this array will hold all courses
+courses = []
+
+with open('courses.csv', 'rb') as f:
+    reader = csv.reader(f)
+
+    # for each row in csv...
+    for row in reader:
+    	# parse course from csv into a dictionary
+    	new_course = {
+    		'school'                : row[0], 
+    		'foreign_couse_subject' : row[1],
+    		'foreign_course_number' : row[2],
+    		'foreign_course_title'  : row[3],
+    		'marist_course_subject' : row[5],
+    		'marist_course_number'  : row[6],
+    		'marist_course_title'   : row[7]
+    	}
+    	# add the dictionary to the array of all courses
+        courses.append(new_course)
+
+valids = 0
+for course in courses:
+	valid = True
+	errors = ""
+
+	# here we can add validation for each column
+
+	# (validate subjects) this example is looking for courses that have a number as their subject instead of a string
+	if (not isSubjectValid(course['foreign_couse_subject'])):
+		valid = False
+		errors = errors + " invalid foreign subj,"
+
+	# if all validations passed...
+	if (valid):
+		# insert query would be done here
+		valids = valids + 1
+
+	# alert user of courses that failed validation (and thus were not added) so they can be added manually
+	else:
+		#remove last comma from error string
+		errors = errors[:-1]	
+		print "[ERROR] "+course['foreign_course_title'] + " : "+errors
+
+
+print ""
+print "Finished!"
+print str(valids) + "/" + str(len(courses)) + " courses added successfully"
