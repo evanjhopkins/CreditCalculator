3c97238cd1ab3001990264adc6070f71d4817379
popcat <moviemaker7@gmail.com>
2015-09-29 23:17:03 -0400

    integrated site with flask

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+	return render_template('index.html')
+
+@app.route('/classes')
+def classes():
+	return render_template('classes.html')
 
 if __name__ == '__main__':
     app.run()
