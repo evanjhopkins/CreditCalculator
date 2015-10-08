c1c4888363a3a85b9d697e0aafd7b6449969c6c8
popcat <moviemaker7@gmail.com>
2015-10-08 19:21:59 -0400

    added /api/college/get api endpoint + added some api utility functions

--- run.py
+++ run.py
@@ -9,7 +9,9 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html')
+	response = api_school()
+	response_obj = json.loads(response)
+	return render_template('index.html', data = response_obj)
 
 @app.route('/classes')
 def classes():
@@ -45,13 +47,42 @@ def api_classes(course_id):
 
 	# if no results were found
 	if (len(classes) < 1):
-		return "{}"
+		return json.dumps(prepare_for_departure({}, [warn("No results found")]), ensure_ascii=False)
 	# if fetching single class
 	if (len(classes) == 1):
-		return json.dumps(classes[0], ensure_ascii=False)
+		return json.dumps(prepare_for_departure(classes[0]), ensure_ascii=False)
 	# else, must be multiple classes
-	return json.dumps({'classes':classes}, ensure_ascii=False)
+	return json.dumps(prepare_for_departure({'classes':classes}), ensure_ascii=False)
 
+@app.route('/api/college/get')
+def api_school():
+	db = MySQLdb.connect("localhost","root","bbemt","creditcalc")
+	cur = db.cursor()
+	stmt = "SELECT * FROM college"
+	colleges = []
+	try:
+		cur.execute(stmt)
+		results = cur.fetchall()
+		for row in results:
+			college_id = row[0]
+			college_name = row[1]
+			colleges.append({'id':college_id, 'name':college_name})
+	except:
+		test = "/api/college/get request failed"
+		return {}
+
+	return json.dumps(prepare_for_departure({'colleges':colleges}) , ensure_ascii=False)
+
+def warn(msg):
+	return {'level':1, 'msg':msg}
+def error(msg):
+	return {'level':0, 'msg':msg}
+
+# content: dictionary ex: {'classes':[{'id':1}, {'id':2}]}
+# errors:  array      ex: [{'level':1, 'msg':'a warning occured!'}, {'level':0, 'msg':'a severe error has occured!'}] 
+def prepare_for_departure(content, errors=[]):
+	return_obj = {'content':content, 'errors':errors }
+	return return_obj
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", debug=True)
+	app.run("0.0.0.0", debug=True)
